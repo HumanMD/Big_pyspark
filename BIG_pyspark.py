bf7740cf59995f341bf942d8624eef8defc6cfc9
human <wthuman.hm@gmail.com>
2021-11-12 12:14:29 +0100

    commit and push 6

--- BIG_pyspark.py
+++ BIG_pyspark.py
@@ -65,7 +65,7 @@ W_schema = ArrayType(
             )),
         ]
     )
-)  # SCHEMA FOR EDGE COLUMN [ W ]
+)  # SCHEMA FOR EDGE COLUMN [ W ] and [ Wi ]
 D_I_schema = ArrayType(
     ArrayType(
         StructType(
@@ -101,6 +101,131 @@ initial_df = spark.createDataFrame(initial_rdd, initial_df_schema)
 
 
 # --------------------------------------------------------------- #
+
+# FUNCTION USED IN irregularGraphRepairing
+
+
+def DeletionRepair(Wi, V, d_elements, cr):
+    Wr1 = []
+    Wr2 = []
+    i = d_elements[0][0]
+    if (d_elements[-1][1], V[i - 1][1]) in cr:
+        for edge in Wi:
+            if edge[1][0] == i and edge[0][0] < i:
+                for h in range(edge[0][0], i):
+                    if (V[h - 1][1], d_elements[0][1]) in cr:
+                        Wr1.append(edge)
+                        break
+            if edge[0][0] < i < edge[1][0]:
+                if (edge[0][1], d_elements[0][1]) in cr:
+                    for l in range(i - 1, edge[1][0]):
+                        if (V[l - 1], edge[1]) in Wi:
+                            Wr2.append(edge)
+                            break
+    Wi = list(set(Wi) - set(Wr1) - set(Wr2))
+    for k in range(i - 1, 0, -1):
+        for j in range(i, len(V) + 1):
+            if (V[k - 1][1], d_elements[0][1]) in cr:
+                if (d_elements[-1][1], V[j - 1][1]) in cr:
+                    if not isReachable(V, Wi, V[k - 1], V[j - 1]):
+                        flag1 = True
+                        for l in range(k - 1, j):
+                            if (V[k - 1], V[l - 1]) in Wi:
+                                flag1 = False
+                                break
+                        flag2 = True
+                        for m in range(k - 1, i):
+                            if (V[m - 1], V[j - 1]) in Wi:
+                                flag2 = False
+                                break
+                        if flag1 or flag2:
+                            Wi.append((V[k - 1], V[j - 1]))
+    return Wi
+
+
+def InsertionRepair(Wi, V, i_elements, cr):
+    Wr1 = []
+    Wr2 = []
+    Wr3 = []
+    Wr4 = []
+    Wr5 = []
+    Wa1 = []
+    Wa2 = []
+    Wa3 = []
+    i = i_elements[0][0]
+    j = i + len(i_elements) - 1
+    only_labels = []
+    for element in i_elements:
+        if element[1] not in only_labels:
+            only_labels.append(element[1])
+    for edge in Wi:
+        if edge[0][0] < i <= edge[1][0] <= j:
+            Wr1.append(edge)
+        if i <= edge[0][0] <= j < edge[1][0]:
+            Wr2.append(edge)
+        if i <= edge[0][0] <= j and i <= edge[1][0] <= j:
+            Wr3.append(edge)
+    Wi = list(set(Wi) - set(Wr1) - set(Wr2) - set(Wr3))
+    for k in range(j + 1, len(V) + 1):
+        if V[k - 1][1] not in only_labels:
+            if (V[i - 2][1], V[k - 1][1]) in cr or (V[i - 2], V[k - 1]) in Wi:
+                if not isReachable(V, Wi, V[j - 1], V[k - 1]):
+                    Wi.append((V[j - 1], V[k - 1]))
+                    Wa1.append((V[j - 1], V[k - 1]))
+    if (V[i - 2][1], V[i][1]) not in cr:
+        Wi.append((V[i - 2], V[i - 1]))
+        Wa2.append((V[i - 2], V[i - 1]))
+    else:
+        for k in range(i - 1, 0, -1):
+            if V[k - 1][1] not in only_labels:
+                if (V[k - 1][1], V[j][1]) in cr or (V[k - 1], V[j]) in Wi:
+                    if not isReachable(V, Wi, V[k - 1], V[i - 1]):
+                        Wi.append((V[k - 1], V[i - 1]))
+                        Wa2.append((V[k - 1], V[i - 1]))
+    for k in range(len(i_elements) - 1):
+        Wa3.append((V[k + i - 1], V[k + i]))
+    if len(Wa3) > 0:
+        for edge in Wa3:
+            Wi.append(edge)
+    for edge in Wa2:
+        for edge2 in Wa1:
+            if edge[1][0] >= i and edge[1][0] <= j:
+                if edge2[0][0] >= i and edge2[0][0] <= j:
+                    Wr4.append((edge[0], edge2[1]))
+    Wi = list(set(Wi) - set(Wr4))
+    if (V[i - 2][1], V[i][1]) not in cr:
+        for edge in Wi:
+            if edge[1][0] > i and edge[0][0] == i - 1:
+                Wr5.append(edge)
+                Wi = list(set(Wi) - set(Wr5))
+    return Wi
+
+
+def isReachable(V, W, s, d):
+    # Mark all the vertices as not visited
+    visited = [False] * (len(V))
+    # Create a queue for BFS
+    queue = []
+    # Mark the source node as visited and enqueue it
+    queue.append(s)
+    visited[s[0] - 1] = True
+    while queue:
+        # Dequeue a vertex from queue
+        j = queue.pop(0)
+        # If this adjacent node is the destination node, then return true
+        if j == d:
+            return True
+        # Else, continue to do BFS
+        for edge in W:
+            if edge[0] == j:
+                if visited[edge[1][0] - 1] == False:
+                    queue.append(edge[1])
+                    visited[edge[1][0] - 1] = True
+    # If BFS is complete without visited d
+    return False
+
+
+# ------------------------------------------- #
 
 # FUNCTIONS TO CONVERT IN UDF
 def create_V(trace):
@@ -200,6 +325,16 @@ def create_I(alignments):
     return I
 
 
+def irregularGraphRepairing(V, W, D, I, cr):
+    if len(D) + len(I) > 0:
+        Wi = W
+        for d_element in D:
+            Wi = DeletionRepair(Wi, V, d_element, cr)
+        for i_element in I:
+            Wi = InsertionRepair(Wi, V, i_element, cr)
+        return Wi
+
+
 # --------------------------- #
 
 # UDF
@@ -207,202 +342,16 @@ udf_create_V = F.udf(lambda trace: create_V(trace), V_schema)
 udf_create_W = F.udf(lambda cr, V: create_W(cr, V), W_schema)
 udf_create_D = F.udf(lambda alignments: create_D(alignments), D_I_schema)
 udf_create_I = F.udf(lambda alignments: create_I(alignments), D_I_schema)
-
+udf_irregularGraphRepairing = F.udf(lambda V, W, D, I, cr: irregularGraphRepairing(V, W, D, I, cr), W_schema)
 
 # ------------------------------------------------------------------------ #
 
-########################################################################################################
-
-# TODO adattare le funzioni per usare dataframe
-
-def irregularGraphReparing(V, W, D, I, cr):
-    Wi = W
-    for d_element in D:
-        Wi = DeletionRepair(Wi, V, d_element, cr)
-        print("Deletion repaired Instance Graph")
-        viewInstanceGraph(V, Wi)
-    for i_element in I:
-        Wi = InsertionRepair(Wi, V, i_element, cr)
-        print("Insertion repaired Instance Graph")
-        viewInstanceGraph(V, Wi)
-    return Wi
-
-
-from IPython import display
-from graphviz import Digraph
-
-
-def viewInstanceGraph(V, W, title="Instance Graph"):
-    # Conversion to string indexes
-    V2 = []
-    W2 = []
-    for node in V:
-        V2.append((str(node[0]), node[1]))
-    for edge in W:
-        W2.append(((str(edge[0][0]), edge[0][1]), (str(edge[1][0]), edge[1][1])))
-
-    dot = Digraph(comment=title, node_attr={'shape': 'circle'})
-    for e in V2:
-        dot.node(e[0], e[1])
-    for w in W2:
-        dot.edge(w[0][0], w[1][0])
-    display.display(dot)
-
-
-def isTraceMatching(V, trace):
-    trace = trace.split(',')
-    flag = True
-    for i in range(len(V)):
-        if trace[i] != V[i][1]:
-            flag = False
-            break
-    return flag
-
-
-def isReachable(V, W, s, d):
-    # Mark all the vertices as not visited
-    visited = [False] * (len(V))
-
-    # Create a queue for BFS
-    queue = []
-
-    # Mark the source node as visited and enqueue it
-    queue.append(s)
-    visited[s[0] - 1] = True
-
-    while queue:
-
-        # Dequeue a vertex from queue
-        j = queue.pop(0)
-
-        # If this adjacent node is the destination node, then return true
-        if j == d:
-            return True
-
-        # Else, continue to do BFS
-        for edge in W:
-            if edge[0] == j:
-                if visited[edge[1][0] - 1] == False:
-                    queue.append(edge[1])
-                    visited[edge[1][0] - 1] = True
-
-    # If BFS is complete without visited d
-    return False
-
-
-def DeletionRepair(Wi, V, d_elements, cr):
-    Wr1 = []
-    Wr2 = []
-    i = d_elements[0][0]
-    if (d_elements[-1][1], V[i - 1][1]) in cr:
-        for edge in Wi:
-            if edge[1][0] == i and edge[0][0] < i:
-                for h in range(edge[0][0], i):
-                    if (V[h - 1][1], d_elements[0][1]) in cr:
-                        Wr1.append(edge)
-                        break
-
-            if edge[0][0] < i and edge[1][0] > i:
-                if (edge[0][1], d_elements[0][1]) in cr:
-                    for l in range(i - 1, edge[1][0]):
-                        if (V[l - 1], edge[1]) in Wi:
-                            Wr2.append(edge)
-                            break
-
-    Wi = list(set(Wi) - set(Wr1) - set(Wr2))
-    for k in range(i - 1, 0, -1):
-        for j in range(i, len(V) + 1):
-            if (V[k - 1][1], d_elements[0][1]) in cr:
-                if (d_elements[-1][1], V[j - 1][1]) in cr:
-                    if not isReachable(V, Wi, V[k - 1], V[j - 1]):
-                        flag1 = True
-                        for l in range(k - 1, j):
-                            if (V[k - 1], V[l - 1]) in Wi:
-                                flag1 = False
-                                break
-                        flag2 = True
-                        for m in range(k - 1, i):
-                            if (V[m - 1], V[j - 1]) in Wi:
-                                flag2 = False
-                                break
-                        if flag1 or flag2:
-                            Wi.append((V[k - 1], V[j - 1]))
-    return Wi
-
-
-def InsertionRepair(Wi, V, i_elements, cr):
-    Wr1 = []
-    Wr2 = []
-    Wr3 = []
-    Wr4 = []
-    Wr5 = []
-    Wa1 = []
-    Wa2 = []
-    Wa3 = []
-    i = i_elements[0][0]
-    j = i + len(i_elements) - 1
-
-    only_labels = []
-    for element in i_elements:
-        if element[1] not in only_labels:
-            only_labels.append(element[1])
-
-    for edge in Wi:
-        if edge[0][0] < i and edge[1][0] >= i and edge[1][0] <= j:
-            Wr1.append(edge)
-        if edge[0][0] >= i and edge[0][0] <= j and edge[1][0] > j:
-            Wr2.append(edge)
-        if edge[0][0] >= i and edge[0][0] <= j and edge[1][0] >= i and edge[1][0] <= j:
-            Wr3.append(edge)
-    Wi = list(set(Wi) - set(Wr1) - set(Wr2) - set(Wr3))
-
-    for k in range(j + 1, len(V) + 1):
-        if V[k - 1][1] not in only_labels:
-            if (V[i - 2][1], V[k - 1][1]) in cr or (V[i - 2], V[k - 1]) in Wi:
-                if not isReachable(V, Wi, V[j - 1], V[k - 1]):
-                    Wi.append((V[j - 1], V[k - 1]))
-                    Wa1.append((V[j - 1], V[k - 1]))
-
-    if (V[i - 2][1], V[i][1]) not in cr:
-        Wi.append((V[i - 2], V[i - 1]))
-        Wa2.append((V[i - 2], V[i - 1]))
-    else:
-        for k in range(i - 1, 0, -1):
-            if V[k - 1][1] not in only_labels:
-                if (V[k - 1][1], V[j][1]) in cr or (V[k - 1], V[j]) in Wi:
-                    if not isReachable(V, Wi, V[k - 1], V[i - 1]):
-                        Wi.append((V[k - 1], V[i - 1]))
-                        Wa2.append((V[k - 1], V[i - 1]))
-
-    for k in range(len(i_elements) - 1):
-        Wa3.append((V[k + i - 1], V[k + i]))
-    if len(Wa3) > 0:
-        for edge in Wa3:
-            Wi.append(edge)
-
-    for edge in Wa2:
-        for edge2 in Wa1:
-            if edge[1][0] >= i and edge[1][0] <= j:
-                if edge2[0][0] >= i and edge2[0][0] <= j:
-                    Wr4.append((edge[0], edge2[1]))
-    Wi = list(set(Wi) - set(Wr4))
-
-    if (V[i - 2][1], V[i][1]) not in cr:
-        for edge in Wi:
-            if edge[1][0] > i and edge[0][0] == i - 1:
-                Wr5.append(edge)
-                Wi = list(set(Wi) - set(Wr5))
-    return Wi
-
-
-# TODO adattare le funzioni fino a qui!!!
-
-# TODO man mano che vengono adattate le funzioni replica main in file tmp.py
 ################################################################################
 df = initial_df.withColumn('V', udf_create_V('Trace')) \
     .withColumn('W', udf_create_W('Causal_relations', 'V')) \
     .withColumn("D", udf_create_D('Alignments')) \
     .withColumn("I", udf_create_I('Alignments')) \
-    .select('Trace_ID', 'V', 'W', 'D', 'I') \
+    .withColumn('Wi', udf_irregularGraphRepairing('V', 'W', 'D', 'I', 'Causal_relations')) \
+    .select('Trace_ID', 'V', 'W', 'D', 'I', 'Wi') \
     .show(5)
 ################################################################################

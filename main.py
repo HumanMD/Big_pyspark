e84fb4e86cbb15b07a34137a5d1bf524049cf743
human <wthuman.hm@gmail.com>
2021-12-03 02:02:01 +0100

    commit and push 16

--- main.py
+++ main.py
@@ -12,14 +12,20 @@ RUN WITH SPARK SUBMIT:
 spark-submit --executor-cores 2 --driver-cores 2 --driver-memory 4g --executor-memory 4g --packages com.databricks:spark-xml_2.12:0.14.0 --py-files /media/sf_cartella_condivisa/progetto/Big_pyspark/utils.zip /media/sf_cartella_condivisa/progetto/Big_pyspark/main.py
 """
 
-from pyspark.sql.session import SparkSession
-from pyspark import Row
-
+from pm4py.objects.log.importer.xes.variants import iterparse as xes_importer
+from pm4py.algo.discovery.dfg import algorithm as dfg_discovery
+import pm4py.algo.discovery.causal.variants.heuristic as cr_discovery
 from pm4py.objects.petri_net.importer.variants import pnml as pnml_importer
 from pm4py.algo.conformance.alignments.petri_net import algorithm as alig
 
+from pyspark.sql.session import SparkSession
+from pyspark import Row
+import pyspark.sql.functions as F
+
 from utils import logRdd as lr
 from utils import genericFunctions as gf
+from utils.genericFunctions import create_V, create_W, create_D, create_I, irregularGraphRepairing
+from utils.schema import V_schema, W_schema, D_I_schema
 
 import time
 
@@ -56,11 +62,34 @@ df = lr \
     .withColumnRenamed('_2', 'trace') \
     .withColumnRenamed('_3', 'alignment')
 
-print("-------------- TRACES AND ALIGNMENTS ---------------\n")
-df.show()
+# CAUSAL RELATIONS (cr)
+log = xes_importer.import_log(rootLocalPathXes + xes[test])
+dfg = dfg_discovery.apply(log)
+CR = cr_discovery.apply(dfg)
+cr = [key for key, val in CR.items() if val > 0.8]
+
+udf_create_V = F.udf(lambda trace: create_V(trace), V_schema)
+udf_create_W = F.udf(lambda V: create_W(cr, V), W_schema)
+udf_create_D = F.udf(lambda alignments: create_D(alignments), D_I_schema)
+udf_create_I = F.udf(lambda alignments: create_I(alignments), D_I_schema)
+udf_irregularGraphRepairing = F.udf(lambda V, W, D, I: irregularGraphRepairing(V, W, D, I, cr), W_schema)
+
+# FINAL DATAFRAME
+final_df = df.withColumn('V', udf_create_V('trace')) \
+    .withColumn('W', udf_create_W('V')) \
+    .withColumn("D", udf_create_D('alignment')) \
+    .withColumn("I", udf_create_I('alignment')) \
+    .withColumn('Wi', udf_irregularGraphRepairing('V', 'W', 'D', 'I')) \
+    .select('trace_id', 'V', 'W', 'D', 'I', 'Wi')
+
+# print("-------------- TRACES AND ALIGNMENTS ---------------\n")
+# df.show()
+
+print("-------------- FINAL DATAFRAME ---------------\n")
+final_df.show()
 
 # TRACE COUNT
-print("-------------- TRACE_COUNT ---------------\n\n" + str(df.count()))
+# print("-------------- TRACE_COUNT ---------------\n\n" + str(df.count()))
 
 # EXEC_TIME
 end = time.time()
